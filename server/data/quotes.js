cf788d505a2959818548ce3a2afc4f0ac26f547c
AnthonyPriceOne2691 <anthonypriceone@gmail.com>
2024-09-07 00:16:56 +0300

    Creating own quotes API node js express server  with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: 'e46695f3',
+    text: 'The only limit to our realization of tomorrow is our doubts of today.',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    id: 'c80917cd',
+    text: 'In the end, we will remember not the words of our enemies, but the silence of our friends.',
+    author: 'Martin Luther King Jr.',
+  },
+  {
+    id: '44e902d0',
+    text: 'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.',
+    author: 'Ralph Waldo Emerson',
+  },
+  {
+    id: 'f34c00fe',
+    text: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+    author: 'Winston Churchill',
+  },
+  {
+    id: 'eadc6cba',
+    text: 'Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world.',
+    author: 'Albert Einstein',
+  },
+  {
+    id: '11dfe610',
+    text: 'The best way to predict your future is to create it.',
+    author: 'Peter Drucker',
+  },
+  {
+    id: '1f6ef3c8',
+    text: "Life is what happens when you're busy making other plans.",
+    author: 'John Lennon',
+  },
+  {
+    id: 'b30cef7a',
+    text: 'Do not wait to strike till the iron is hot, but make it hot by striking.',
+    author: 'William Butler Yeats',
+  },
+  {
+    id: '49ae45cb',
+    text: 'It does not matter how slowly you go as long as you do not stop.',
+    author: 'Confucius',
+  },
+  {
+    id: '6f42d12b',
+    text: 'Our greatest glory is not in never falling, but in rising every time we fall.',
+    author: 'Confucius',
+  },
+];
+
+module.exports = quotes;
